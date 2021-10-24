67cf3557e307a03ea5a6d1165ae8deb4a5b3384e
Achgelis <coccejus26@gmx.de>
2021-10-24 10:48:53 +0200

    2.0

--- common/defines.lua
+++ common/defines.lua
@@ -523,7 +523,7 @@ diplomacy = {
 	
 	-- effects of manually releasing a nation
 	RELEASE_NATION_PRESTIGE = -10,
-	RELEASE_NATION_INFAMY = -2,
+	RELEASE_NATION_INFAMY = -1,
 	
 	INFAMY_CLEAR_UNION_SPHERE = 0, -- Infamy for asserting hegemony
 	
